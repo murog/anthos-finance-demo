23922980fbd532aca1d5fe2ded4a2a10cbe3c7d5
Guillermina Muro <guillermina.muro@gmail.com>
2020-04-06 11:35:55 -0700

    default credentials spec

--- e2e/cypress/integration/login_spec.js
+++ e2e/cypress/integration/login_spec.js
@@ -1,5 +1,46 @@
-describe('The Login Page', function() {
+describe('Login Page', function() {
   it('successfully loads', function() {
     cy.visit('/login')
   })
 })
+
+describe('Default Credentials on Form Submission', function() {
+  const username = 'testuser'
+  const password = 'password'
+  const name = 'Test User'
+  const expectedBalance = '$6, 026.20'
+
+  beforeEach(function() {
+    cy.visit('login')
+
+    cy.get('input[name=username]').clear().type(username)
+    cy.get('input[name=password]').clear().type(password)
+    cy.get('form').submit()
+  })
+
+  it('redirects to home', function() {
+    // redirect to home page
+    cy.url().should('include', '/home')
+  })
+
+  it('sees correct username', function() {
+    // TODO: class "account-user-name" should be ID
+    cy.get('#accountDropdown').contains(name)
+  })
+
+  // TODO: blocked until id implemented
+  it.skip('sees correct balance', function() {
+    // TODO: span should have id "current-balance"
+    cy.get('#current-balance').contains(expectedBalance)
+  })
+
+})
+
+describe('Bad Credentials on Form Submission', function() {
+
+
+  it('given bad credentials, fails', function() {
+
+  })
+})
+
